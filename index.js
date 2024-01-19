051f85b182c758e51e828217afa91ba4e70913f0
Parvani <120269601+parvanhiii@users.noreply.github.com>
2024-01-19 11:00:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride=require('method-override')
+const { v4: uuid } = require('uuid');
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+let comments = [
+    {
+        id:uuid(),
+        username: 'Pepcode',
+        comment: 'lol'
+    },
+    {    id:uuid(),
+        username: 'Parv',
+        comment: 'lmao'
+    },
+    {   id:uuid(),
+        username: 'Lara',
+        comment: 'haha'
+    },
+    {   id:uuid(),
+        username: 'Magic',
+        comment: 'yooo'
+    }
+]
+app.get('/comments', (req, res) => {
+    res.render('comments/index',{comments})
+})
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new',{comments})
+})
+app.post('/comments', (req, res) => {
+    const{username,comment}=req.body;
+    comments.push({username,comment,id:uuid() })
+    res.redirect("/comments");
+})
+
+app.get('/comments/:id', (req, res) =>{
+    const{id} =req.params;
+    const comment =comments.find(c => c.id===id);
+    res.render('comments/show',{comment}); //can be named anything like details
+})
+
+app.get('/comments/:id/edit', (req, res) =>{
+    const{id} =req.params;
+    const comment =comments.find(c => c.id=== id);
+    res.render('comments/edit',{comment}); 
+})
+
+app.patch('/comments/:id', (req, res) =>{
+    const{id} =req.params;
+    const newCommentText= req.body.comment;
+    const foundComment =comments.find(c => c.id===(id));
+    foundComment.comment=newCommentText;
+    res.redirect('/comments'); 
+})
+app.delete('/comments/:id', (req, res) =>{
+    const{id} =req.params;
+    const foundComment =comments.find(c => c.id===(id));
+    comments =comments.filter(c => c.id!==(id));
+    res.redirect('/comments'); 
+})
+
+app.get('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send("ok here")
+})
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`ok here are your ${qty} ${meat} tacos`)
+})
+app.listen(3000, () => {
+    console.log("ON PORT 3000")
+})
